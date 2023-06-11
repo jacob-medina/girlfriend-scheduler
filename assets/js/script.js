8a75de7bea556457565a0bb0286312690f20dc2b
Jacob Medina <jacobmedina1618@gmail.com>
2023-06-11 15:56:04 -0400

    completed acceptance criteria functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,104 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+const minTimeBlock = 9;
+const maxTimeBlock = 17;
+const updateTimeSeconds = 60;  // update display every 60 seconds
+var updateInterval;
+
+
+// converts 24 hour time to 12 hour time
+function to12Hour(time) {
+  if (time < 12) { return time + " AM"; }
+
+  else if (time === 12) { return "12 PM"; }
+
+  else { return (time - 12) + " PM"; }
+}
+
+
+function saveTimeBlock(event) {
+  const timeBlock = $(event.target).parent();
+  const textArea = timeBlock.find('.description');
+
+  localStorage.setItem(timeBlock.attr('id'), textArea.val());
+}
+
+
+// generates time block elements and their child elements
+function generateTimeBlocks() {
+  for (var time = minTimeBlock; time <= maxTimeBlock; time++) {
+    var timeBlock = $('<section>');
+    timeBlock.attr('id', 'hour-' + time)
+    timeBlock.attr('class', 'row time-block');
+    timeBlock.append('<div class="col-2 col-md-1 hour text-center py-3">' + to12Hour(time) + '</div><textarea class="col-8 col-md-10 description" rows="3"> </textarea><button class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true"></i></button>');
+
+    var saveButton = timeBlock.find('.saveBtn');
+    saveButton.on('click', saveTimeBlock);
+
+    $('#time-block-container').append(timeBlock);
+  }
+  
+}
+
+
+// updates time block colors based on current time
+function updateTimeBlockColors() {
+  var timeBlocks = $('.time-block');
+
+  for (var timeBlock of timeBlocks) {
+    timeBlock = $(timeBlock);
+
+    const hours = Number(timeBlock.attr('id').slice(5));  // get hour block as number
+    const blockTime = dayjs().startOf('day').add(hours, 'hour');  // time block as a Date
+    const timeDifference = blockTime.diff(dayjs().startOf('hour'), 'hour');  // difference between blockTime and current time
+
+    timeBlock.attr('class', 'row time-block');  // reset time block classes
+
+    // set class based on time difference
+    if (timeDifference < 0) { timeBlock.addClass('past'); }
+    else if (timeDifference === 0) { timeBlock.addClass('present'); }
+    else { timeBlock.addClass('future'); }
+
+  }
+}
+
+
+// update text that displays current date
+function updateCurrentDate() {
+  $('#currentDay').text(dayjs().format('dddd, MMMM D'));
+}
+
+
+// update time-based elements
+function update() {
+  updateTimeBlockColors();
+  updateCurrentDate();
+}
+
+
+// updates time blocks with local storage data (if any)
+function pullFromLocalStorage() {
+  var timeBlocks = $('.time-block');
+
+  for (var time = minTimeBlock; time <= maxTimeBlock; time++) {
+    const text = localStorage.getItem('hour-' + time);
+
+    if (text === null || text === undefined) { continue; }
+
+    const timeBlock = $('#hour-' + time);
+    timeBlock.find('.description').text(text);
+  }
+}
+
+
+// executes a single time once the webpage loads
+function init() {
+  generateTimeBlocks();
+
+  // start update loop
+  updateInterval = setInterval(update, updateTimeSeconds * 1000);
+  update();
+
+  pullFromLocalStorage();
+}
+
+
+$(init);
